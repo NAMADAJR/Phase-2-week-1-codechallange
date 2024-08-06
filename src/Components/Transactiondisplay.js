97204798a90e1d7296248d30495adc53fd8d723d
Namada Junior <namada871@gmail.com>
2024-08-06 17:52:35 +0300

    Displays the transaction on the UI

---
+++ src/Components/Transactiondisplay.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Transaction from './Transaction'
+
+const Transactiondisplay = () => {
+
+  const [transaction , setTransaction] = useState([])
+
+  useEffect(() => {
+   fetch("http://localhost:3000/transactions")
+   .then(res => res.json())
+   .then(data => setTransaction(data))
+   .catch(error => console.log(error))
+  },[])
+
+  const displayTransaction = transaction.map(transaction => { 
+      return (
+          <div className='Transaction'>
+          <Transaction key={transaction.id} transaction={transaction}/>
+          </div>
+      )
+   })
+
+
+  return (
+      <div>
+          {displayTransaction}
+      </div>
+  )
+}
+
+export default Transactiondisplay
