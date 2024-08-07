e2ab1c7ee11e18afc5303b09436fbfabd374d898
Namada Junior <namada871@gmail.com>
2024-08-08 00:08:47 +0300

    Adds the seach component to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Inputfield from "./Components/Inputfield";
+import Search from "./Components/Search";
 import Transactiondisplay from "./Components/Transactiondisplay";
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((res) => res.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
   return (
     <div>
       <div>
-        <Inputfield />
+        <Inputfield addTransaction={addTransaction} />
       </div>
       <div>
-        <Transactiondisplay />
+        <Transactiondisplay transactions={transactions} />
+      </div>
+      <div>
+        <Search transactions={transactions} />
       </div>
     </div>
   );
 }
 
 export default App;
+
