e1fdd7628002e87d92c7c69930a64685f61fd02f
Namada Junior <namada871@gmail.com>
2024-08-07 23:11:11 +0300

    Displays new search feature

--- src/Components/Inputfield.js
+++ src/Components/Inputfield.js
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Search from "./Search";
 
 const Form = () => {
-  // Initialize the state to hold form data
   const [formData, setFormData] = useState({
     category: "",
     description: "",
     amount: 0,
   });
- 
-  // Handle changes to input fields
+  const [transactions, setTransactions] = useState([]);
+
   const handleOnChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
 
-    // Update the state with the new value
     setFormData({ ...formData, [name]: value });
   };
 
-  // Handle form submission
   const handleOnSubmit = (event) => {
     event.preventDefault();
 
@@ -30,8 +28,17 @@ const Form = () => {
       body: JSON.stringify(formData),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setTransactions([...transactions, data]);
+        setFormData({ category: "", description: "", amount: 0 });
+      });
   };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((res) => res.json())
+      .then((data) => setTransactions(data));
+  }, []);
 
   return (
     <div className="form">
@@ -76,6 +83,8 @@ const Form = () => {
           </div>
         </form>
       </div>
+
+      <Search transactions={transactions} />
     </div>
   );
 };
