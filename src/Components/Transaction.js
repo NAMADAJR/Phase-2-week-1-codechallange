6f1346ed119025edcbc0ceac3af896d4e563a7b1
Namada Junior <namada871@gmail.com>
2024-08-08 00:15:32 +0300

    Adds correct class name  for styling

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Transaction = ({transaction}) => {
   return (
-    <div className='Transaction'>
-      <h1>Cartegory:{transaction.category}</h1>
+    <div className="transaction">
+      <h2>Cartegory:{transaction.category}</h2>
       <h3>Description:{transaction.description}</h3>
       <h3>Amount:{transaction.amount}</h3>
     </div>
