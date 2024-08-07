74d08d1fe6ec6a8be8c035b38968d82724788f74
Namada Junior <namada871@gmail.com>
2024-08-08 00:12:53 +0300

    Adds classname for styling

--- src/Components/Transactiondisplay.js
+++ src/Components/Transactiondisplay.js
@@ -1,32 +1,26 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Transaction from './Transaction'
+import React from "react";
+import { useState, useEffect } from "react";
+import Transaction from "./Transaction";
 
 const Transactiondisplay = () => {
-
-  const [transaction , setTransaction] = useState([])
+  const [transaction, setTransaction] = useState([]);
 
   useEffect(() => {
-   fetch("http://localhost:3000/transactions")
-   .then(res => res.json())
-   .then(data => setTransaction(data))
-   .catch(error => console.log(error))
-  },[])
+    fetch("http://localhost:3000/transactions")
+      .then((res) => res.json())
+      .then((data) => setTransaction(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-  const displayTransaction = transaction.map(transaction => { 
-      return (
-          <div className='Transaction'>
-          <Transaction key={transaction.id} transaction={transaction}/>
-          </div>
-      )
-   })
-
-
-  return (
+  const displayTransaction = transaction.map((transaction) => {
+    return (
       <div>
-          {displayTransaction}
+        <Transaction key={transaction.id} transaction={transaction} />
       </div>
-  )
-}
+    );
+  });
 
-export default Transactiondisplay
+  return <div className="transaction-display">{displayTransaction}</div>;
+};
+
+export default Transactiondisplay;
